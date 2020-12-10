a735bf9de56a17e0db4093e9ce4a3ab75862a1be
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2020-12-10 18:17:18 -0500

    Initial

---
+++ flask_app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mongoengine import MongoEngine
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_user,
+    logout_user,
+    login_required,
+)
+from flask_bcrypt import Bcrypt
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route("/")
+def default_route():
+    return "Venmo Clone"
