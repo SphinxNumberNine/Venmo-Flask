cad658e24ca6af0ff264dc5f79c88d618f4ebfff
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2020-12-14 18:50:58 -0500

    added heroku stuff

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -33,6 +33,7 @@ def page_not_found(e):
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_pyfile("config.py", silent=False)
+    app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
     if test_config is not None: 
         app.config.update(test_config)
     db.init_app(app)
